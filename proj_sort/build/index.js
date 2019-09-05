a273c17d8a27e6762e9e2bc0b1b9bbca903e4a9b
Pob <590650@gmail.com>
2019-09-05 23:36:56 +0700

    bubble sort algo

--- proj_sort/build/index.js
+++ proj_sort/build/index.js
@@ -1,3 +1,17 @@
 "use strict";
-console.log('Hi There');
-console.log('Test TEST');
+var myArr = [10, -1, 5, 0];
+var sorter = function (arr) {
+    var len = arr.length;
+    while (len > 0) {
+        for (var i = 0; i < len - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                var left = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = left;
+            }
+        }
+        len--;
+    }
+};
+sorter(myArr);
+console.log(myArr);
--- proj_sort/src/index.ts
+++ proj_sort/src/index.ts
@@ -1,3 +1,18 @@
-console.log('Hi There')
+const myArr = [10, -1, 5, 0]
 
-console.log('Test TEST')
+const sorter = (arr: number[]): void => {
+  let len = arr.length
+  while (len > 0) {
+    for (let i = 0; i < len - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        const left = arr[i + 1]
+        arr[i + 1] = arr[i]
+        arr[i] = left
+      }
+    }
+    len--
+  }
+}
+
+sorter(myArr)
+console.log(myArr)
