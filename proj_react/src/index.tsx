6930304cc1cc195e096c5e90473d8551a5df16db
Pob <590650@gmail.com>
2019-11-14 17:48:31 +0700

    begin to write first component

--- proj_react/src/index.tsx
+++ proj_react/src/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-function App(): JSX.Element {
-  return <div>This is Hello World</div>
-}
+import { App } from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
