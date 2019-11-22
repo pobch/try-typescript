f49000a431e09b532c1faef658cd42febf9e9c6f
Pob <590650@gmail.com>
2019-11-22 16:28:04 +0700

    edit text

--- proj_react/src/index.tsx
+++ proj_react/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 function App(): JSX.Element {
-  return <div>This is Hello World</div>
+  return <div>This is Hello Worlddddddd</div>
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
